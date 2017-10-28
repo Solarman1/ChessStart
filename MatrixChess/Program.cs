180d3e9a639ef18c7a39855bd96914caf234cdc7
пк <arthurthehead@gmail.com>
2017-10-28 21:29:24 +0300

    End first part

--- MatrixChess/Program.cs
+++ MatrixChess/Program.cs
@@ -15,13 +15,29 @@ namespace MatrixChess
 
             int[,] matchess = new int[n, m];
 
-            for(int i = 0; i<matchess.Length; i++)
+            for (int i = 0; i < n; i++)
             {
-                for (int j = 0; j < matchess.Length; j++)
-                    matchess[i, j] = o;
-                    
+                for (int j = 0; j < m; j++)
+                {
+                    if (o == 1)
+                        matchess[i, j] = 1;
+                    else
+                        matchess[i, j] = 0;
+                    o = -o;
+                }
             }
-         
+            
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                { 
+                    Console.Write(matchess[i, j]);                
+                  
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
