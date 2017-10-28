1d01f84faaca0f88c0a28886352875811d72307f
пк <arthurthehead@gmail.com>
2017-10-28 21:06:15 +0300

    Start

---
+++ MatrixChess/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MatrixChess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter matrix size: ");
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+
+            int o = 1;
+
+            int[,] matchess = new int[n, m];
+         
+        }
+    }
+}
